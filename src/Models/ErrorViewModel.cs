ef211c3e0899c1812832c1a8c25d1a91ea0f0506
PaddiM8 <paddim8@pm.me>
2019-05-12 14:05:28 +0200

    ASP.NET

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InfoQuizMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
